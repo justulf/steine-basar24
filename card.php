58b6d4350971c98cd2afc9e4efeaa016505a014d
Leo <leonardhach@gmail.com>
2021-04-18 10:22:53 +0200

    Es geht

--- card.php
+++ card.php
@@ -1,9 +1,24 @@
-<div class="card">
-  <div class="card-body">
-    <?php echo '<img src="data:image;base64,'.base64_encode($row['productsPicture']).'" class="card-img-top">' ?> 
-    <hr>
-    <?php echo $row['productsName'] ?>
-    <a href="" class="btn btn-primary">Details</a>
-    <a href="" class="btn btn-primary">In den Warenkorb</a>
+<head>
+  <link rel="stylesheet" href="css\style.css">
+</head>
+<body>
+  <div class="container">
+    <div class="card mt-6" style="width:300px">
+      <div class="px-3">
+        <div class="card-overlay">
+          <h5 class="card-title text-center"><?php echo $row['productsName'] ?></h5>
+          <p><?php echo $row['productsShortDescription'] ?></p>
+          <p><?php echo $row['productsPrice'] ?></p>
+          <p><?php echo $row['productsRating'] ?></p>
+          <a href="" class="btn btn-primary">Details</a>
+          <a href="" class="btn btn-primary">In den Warenkorb</a>
+        </div>
+      </div>
+      <div class="card-body">
+        <?php echo '<img src="data:image;base64,'.base64_encode($row['productsPicture']).'" >' ?>
+        <hr>
+        <h5 class="card-title text-center"><?php echo $row['productsName'] ?></h5>
+      </div>
+    </div>
   </div>
-</div>
+</body>
